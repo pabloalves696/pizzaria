9d5eb6ec06d11d6b92105727a68fb72e423ebf53
pabloalves696 <118540276+pabloalves696@users.noreply.github.com>
2024-02-11 19:31:37 +0000

    up
    up

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const scrollReveal = scrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+})
+
+scrollReveal.reveal(
+`.sobre-imagem, .sobre-textos, .button, .card-pizza, .depoimento, .rodape`,
+{
+  interval: 100,
+}
+)
